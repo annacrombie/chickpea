289efffd5b174892714fd7a1d0f8a4983af19181
Stone Tickle <lattis@mochiro.moe>
2019-04-22 13:17:12 -0500

    spec it out

--- spec/cstash_spec.rb
+++ spec/cstash_spec.rb
@@ -3,7 +3,29 @@ RSpec.describe Cstash do
     expect(Cstash::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:stash) {
+    Cstash::Stash.new(
+      a: {
+        b: {
+          c: {
+            d: 1
+          }
+        },
+        bb: false
+      }
+    )
+  }
+
+  it 'has working accessors' do
+    expect(stash.a.b.c.d).to eq(1)
+    expect(stash.a.bb?).to be(false)
+    expect(stash.a).not_to respond_to(:b=)
+  end
+
+  it 'allows you to set some attributes' do
+    expect { stash.a.bb = 1 }.to raise_exception(TypeError)
+    expect { stash.a.bb = false }.not_to raise_exception
+    expect { stash.a.b.c.d = 1.1 }.to raise_exception(TypeError)
+    expect { stash.a.b.c.d = 3 }.not_to raise_exception
   end
 end

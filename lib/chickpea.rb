40d50b5887fd410ce3f20befe08176a4ad2ef22b
Stone Tickle <lattis@mochiro.moe>
2019-05-04 08:03:15 -0500

    fix bug where ? accessor isn't updated
    dry up method logic

--- lib/chickpea.rb
+++ lib/chickpea.rb
@@ -8,38 +8,50 @@ module Chickpea
   end
 
   class<<self
+    def def_accessors(klass, key, nv)
+      (
+        case nv
+        when true, false
+          [[:"#{key}?", -> { nv }]]
+        else
+          []
+        end + [[:"#{key}", -> { nv }]]
+      ).each do |(meth, block)|
+        klass.define_method(meth, &block)
+      end
+    end
+
+    def check_type(obj, nv)
+      case obj
+      when true, false
+        unless nv == true || nv == false
+          raise(TypeError.new(nv, :Boolean))
+        end
+      else
+        unless nv.is_a?(obj.class)
+          raise(TypeError.new(nv, obj.class.name))
+        end
+      end
+    end
+
     def make_stash(hash)
       klass = Class.new
 
-      hash.map do |key, val|
+      hash.each do |key, val|
         obj =
           if val.is_a?(Hash)
             make_stash(val)
           else
-            val.tap do |v|
-              case v
-              when true, false
-                klass.define_method(:"#{key}?") { obj }
-                klass.define_method(:"#{key}=") do |nv|
-                  unless nv == true || nv == false
-                    raise(TypeError.new(nv, :Boolean))
-                  end
+            klass.define_method(:"#{key}=") do |nv|
+              Chickpea.check_type(val, nv)
 
-                  define_singleton_method(key) { nv }
-                end
-              else
-                klass.define_method(:"#{key}=") do |nv|
-                  unless nv.is_a?(obj.class)
-                    raise(TypeError.new(nv, obj.class.name))
-                  end
-
-                  define_singleton_method(key) { nv }
-                end
-              end
+              Chickpea.def_accessors(klass, key, nv)
             end
+
+            val
           end
 
-        klass.define_method(key) { obj }
+        Chickpea.def_accessors(klass, key, obj)
       end
 
       klass.define_method(:to_h) do
